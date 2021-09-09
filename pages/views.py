d2d839beee2453c3a957cedbede047271bf56b09
Bob Stofko <bob.stofko@gmail.com>
2021-09-09 13:54:26 -0500

    added cards to display api list

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, "home.html", {})
+  import json
+  import requests
+
+  api_request = requests.get("https://api.openbrewerydb.org/breweries?per_page=50&by_city=san_antonio")
+  query = ""
+
+  try:
+      api = json.loads(api_request.content)
+  except Exception as e:
+      api = "Error..."
+
+  return render(request, "home.html", {'api': api})
 
 def about(request):
   my_name = "Hello, my name is Bob"
